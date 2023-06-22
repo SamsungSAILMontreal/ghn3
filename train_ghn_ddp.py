e833cce2c61683d4da91069c431de1462cb7f19b
Boris Knyazev <borknyaz@gmail.com>
2023-06-22 16:30:38 -0400

    support cifar-10 and ghn2

--- train_ghn_ddp.py
+++ train_ghn_ddp.py
@@ -26,6 +26,10 @@ Example:
 
     # Use eval_ghn_imagenet.py to evaluate the trained GHN-3 model on ImageNet.
 
+    # To train GHN-3-T/m8 on CIFAR-10:
+    python train_ghn_ddp.py -n -v 50 --ln -m 8 --name ghn3tm8-c10 --hid 64 --layers 3 --opt adamw --lr 4e-4 --wd 1e-2 \
+     --scheduler cosine-warmup --debug 0
+
 """
 
 
@@ -44,6 +48,8 @@ def main():
     parser.add_argument('--layers', type=int, default=3, help='number of layers in GHN-3')
     parser.add_argument('--heads', type=int, default=8, help='number of self-attention heads in GHN-3')
     parser.add_argument('--compile', type=str, default=None, help='use pytorch2.0 compilation for potential speedup')
+    parser.add_argument('--ghn2', action='store_true', help='train GHN-2, also can use'
+                                                            ' https://github.com/facebookresearch/ppuda to train GHN-2')
 
     ddp = setup_ddp()
     args = None
@@ -62,32 +68,32 @@ def main():
             'Therefore, this repo is not supporting DataParallel anymore as it complicates some steps. '
             'nn.DistributedDataParallel is used if this script is called with torchrun (see examples on top).')
     is_imagenet = args.dataset.startswith('imagenet')
-    if not is_imagenet:
-        log('WARNING: only ImageNet was tested, but the specified dataset is {}'.format(args.dataset))
 
     log('loading the %s dataset...' % args.dataset.upper())
     train_queue, _, num_classes = image_loader(args.dataset,
                                                args.data_dir,
-                                               im_size=224,
+                                               im_size=224 if is_imagenet else 32,
                                                test=False,
                                                batch_size=args.batch_size,
                                                num_workers=args.num_workers,
                                                seed=args.seed,
                                                verbose=ddp.rank == 0)
 
-    is_ghn2 = False  # to train GHN-2, use https://github.com/facebookresearch/ppuda
     hid = args.hid
     config = {'max_shape': args.max_shape, 'num_classes': num_classes, 'hypernet': args.hypernet,
               'decoder': args.decoder, 'weight_norm': args.weight_norm, 've': args.virtual_edges > 1,
-              'layernorm': args.ln, 'hid': hid, 'layers': args.layers, 'heads': args.heads, 'is_ghn2': is_ghn2}
+              'layernorm': args.ln, 'hid': hid, 'layers': args.layers, 'heads': args.heads, 'is_ghn2': args.ghn2}
 
-    if args.max_shape != ((hid * 2, hid * 2, 16, 16) if is_ghn2 else (hid, hid, 16, 16)):
-        log('WARNING: max_shape is different from the default GHN2/GHN3 values. ')
+    s = 16 if is_imagenet else 11
+    default_max_shape = (hid * 2, hid * 2, s, s) if args.ghn2 else (hid, hid, s, s)
+    if args.max_shape != default_max_shape:
+        log('WARNING: max_shape {} is different from the default GHN2/GHN3 max_shape {}. '.format(
+            args.max_shape, default_max_shape))
 
     ghn = GHN3(**config, debug_level=args.debug)
     graphs_queue, sampler = DeepNets1MDDP.loader(args.meta_batch_size // (ddp.world_size if ddp.ddp else 1),
                                                  dense=ghn.is_dense(),
-                                                 wider_nets=True,
+                                                 wider_nets=is_imagenet,
                                                  split=args.split,
                                                  nets_dir=args.data_dir,
                                                  virtual_edges=args.virtual_edges,
@@ -96,10 +102,13 @@ def main():
                                                  verbose=ddp.rank == 0,
                                                  debug=args.debug > 0)
 
+    lr_scheduler = 'mstep' if args.scheduler is None else args.scheduler
+    scheduler_args = {'milestones': args.lr_steps, 'gamma': args.gamma} if lr_scheduler == 'mstep' else None
     trainer = Trainer(ghn,
                       opt=args.opt,
                       opt_args={'lr': args.lr, 'weight_decay': args.wd},
-                      scheduler=args.scheduler,
+                      scheduler=lr_scheduler,
+                      scheduler_args=scheduler_args,
                       n_batches=len(train_queue),
                       grad_clip=args.grad_clip,
                       device=args.device,
@@ -107,7 +116,7 @@ def main():
                       amp=args.amp,
                       amp_min_scale=1024,       # this helped stabilize AMP training
                       amp_growth_interval=100,  # this helped stabilize AMP training
-                      predparam_wd=3e-5,
+                      predparam_wd=0 if args.ghn2 else 3e-5,
                       label_smoothing=0.1 if is_imagenet else 0.0,
                       save_dir=args.save,
                       ckpt=args.ckpt,
